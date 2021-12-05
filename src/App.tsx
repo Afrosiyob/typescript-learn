e299eebf1ef99b56543d6df025234b38ae98a435
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-12-05 21:53:04 +0500

    6 - video objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { e_links } from "./enums/enums";
+import { UserType } from "./types/types";
+
+console.log(e_links.telegram);
 
 function App() {
+  const createUser = (name?: string, age?: string | number) => {
+    console.log(name, age);
+  };
+
+  createUser("leo", 24);
+
+  const createSkills = (name: string, ...skills: Array<string>) => {
+    console.log(name, skills);
+  };
+
+  createSkills("leo", "js", "html", "css");
+
+  let user: UserType = {
+    name: "leo",
+    age: 23,
+  };
+
+  console.log(user);
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/enums/enums.ts
@@ -0,0 +1,4 @@
+export const enum e_links {
+    youtube="www.youtube.com",
+    telegram="www.telegram.com"
+}
