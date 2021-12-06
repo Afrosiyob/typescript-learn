7b08290f24cd7ae035e657b8f79a1a2a7a931995
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2021-12-06 13:55:25 +0500

    add
    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import { e_links } from "./enums/enums";
 import { UserType } from "./types/types";
@@ -24,24 +24,26 @@ function App() {
     age: 23,
   };
 
+  const array:[any] = [45];
+   
+  console.log(array);
+
   console.log(user);
+
+
+const getter = <T extends unknown>(data: T):T => data
+
+console.log(getter("w").length);
+
+
+  
+const addToArray = (event:React.ChangeEvent<HTMLInputElement>) => {
+  console.log(event.target?.value);
+}
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={addToArray} />
     </div>
   );
 }
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,5 @@
+ export interface UserInterface {
+      name: string,
+      age?:number|string,
+      [propName:string]: any
+ }
