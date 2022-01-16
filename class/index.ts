e9030049a784cbf8cf63b0197365d1e537cd63b4
Afrosiyob <45900728+Afrosiyob@users.noreply.github.com>
2022-01-16 20:27:20 +0500

    add
    add

--- class/index.ts
+++ class/index.ts
@@ -2,8 +2,11 @@ export default class User {
   public name: string;
   public age: number;
 
-  constructor(name: string, age: number) {
+  public login: string | undefined;
+
+  constructor(name: string, age: number, login?: string) {
     this.name = name;
     this.age = age;
+    this.login = login;
   }
 }
